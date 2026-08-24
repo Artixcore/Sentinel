fa97892fdf2f583af9e6bb1b456d532ad8a5f111
Artixcore <150189533+Artixcore@users.noreply.github.com>
2026-08-24 22:52:11 +0600

    Name scheduled monitor dispatch event

--- apps/api/routes/console.php
+++ apps/api/routes/console.php
@@ -2,4 +2,15 @@
 use App\Jobs\RunWebsiteCheck;
 use App\Models\Monitor;
 use Illuminate\Support\Facades\Schedule;
-Schedule::call(function(){ Monitor::query()->where('enabled',true)->each(function(Monitor $monitor){ if(!$monitor->last_checked_at || $monitor->last_checked_at->addSeconds($monitor->interval_seconds)->isPast()) RunWebsiteCheck::dispatch($monitor->id); }); })->everyMinute()->withoutOverlapping();
+Schedule::call(function () {
+    Monitor::query()
+        ->where('enabled', true)
+        ->each(function (Monitor $monitor) {
+            if (! $monitor->last_checked_at || $monitor->last_checked_at->addSeconds($monitor->interval_seconds)->isPast()) {
+                RunWebsiteCheck::dispatch($monitor->id);
+            }
+        });
+})
+    ->name('dispatch-due-website-checks')
+    ->everyMinute()
+    ->withoutOverlapping();
